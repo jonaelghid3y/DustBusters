79c0eb1423673a8986f923ff000dd40a8b26901f
jonaelghid3y <jonaelghidey@hotmail.com>
2023-09-24 15:16:23 +0200

    some things added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,80 +1,106 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 function Nav() {
+    const [isopen, setIsopen] = useState(false);
+
     return (
         <StyledNav>
             <StyledLoggoDiv>
-                <StyledLogoText1>
-                    Dust
-                </StyledLogoText1>
-                <StyledLogoText2>
-                    Busters
-                </StyledLogoText2>
-                
+                <StyledLogoText1>Dust</StyledLogoText1>
+                <StyledLogoText2>Busters</StyledLogoText2>
             </StyledLoggoDiv>
-
-            <StyledLänkcontainer>
-                <Link to="/"className="länkar">Hem</Link>
-                <Link to="/Bookings"className="länkar">Boka</Link>
-                <Link to="/Services"className="länkar">Tjänster</Link>
-                <Link to="/Aboutus"className="länkar" id="länkknapp">Login</Link>
+            <StyledHamburgerIcon onClick={() => setIsopen(!isopen)} size={30} />
+            <StyledLänkcontainer isopen={isopen}>
+                <Link to="/" className="links">Hem</Link>
+                <Link to="/Bookings" className="links">Boka</Link>
+                <Link to="/Services" className="links">Tjänster</Link>
+                <Link to="/Aboutus" id="linkButton">Login</Link>
             </StyledLänkcontainer>
         </StyledNav>
     )
 }
+
 const StyledNav = styled.nav`
-height: 100%;
- display: flex;
- align-items: center;
- justify-content: space-between;
-font-weight: bold;
-font-family: 'Poppins', sans-serif;
-padding: 0 30px;
-background-color: #232323;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-weight: bold;
+    font-family: 'Poppins', sans-serif;
+    padding: 0 30px;
+    background-color: #232323;
+`;
 
-
-`
 const StyledLänkcontainer = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-gap: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 25px;
 
-.länkar {
-    text-decoration: none; 
-    color: white; 
-    font-size: 18px; 
-    transition: color 0.3s ease; 
-  }
+    @media (max-width: 768px) {
+        position: absolute;
+        top: 60px;
+        right: 0;
+        padding: 15px;
+        background-color: #232323;
+        width: 30%;
+        flex-direction: column;
+        display: ${props => (props.isopen ? 'flex' : 'none')};
+    }
 
-  .länkar:hover {
-    color: #FFD530; 
+    .links {
+        text-decoration: none; 
+        color: white; 
+        font-size: 18px; 
+        transition: color 0.3s ease; 
+    }
 
-  .länkar:active {
-    color: #e5e5e5; 
-  }
-  #länkknapp{
-    background-color: #FFD530;
-    border-radius: 5px;
-    padding: 2px 7px;
-    color: black;
-  }
-`
+    .links:hover {
+        color: #FFD530; 
+    }
+
+    .links:active {
+        color: #e5e5e5; 
+    }
+  
+    #linkButton {
+        background-color: #FFD530;
+        border-radius: 5px;
+        padding: 2px 7px;
+        color: black;
+        text-decoration: none; 
+        font-size: 18px; 
+        transition: color 0.3s ease;
+    }
+`;
+
 const StyledLoggoDiv = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 const StyledLogoText1 = styled.h2`
-font-size:26px;
-color: white;
-`
+    font-size:26px;
+    color: white;
+`;
+
 const StyledLogoText2 = styled.h2`
-font-size:26px;
-color: #FFD530;
+    font-size:26px;
+    color: #FFD530;
+`;
 
-`
+const StyledHamburgerIcon = styled(FaBars)`
+    cursor: pointer;
+    color: #FFD530;
+    
+    @media (min-width: 769px) {
+        display: none;
+    }
+`;
 
-export default Nav
+export default Nav;
--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { AiFillSafetyCertificate,AiFillStar } from 'react-icons/ai';
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function LandingPage() {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Number of slides to show at a time
+    draggable: true,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1, // Number of slides to show on mobile devices
+        },
+      },
+    ],
+  }
+
   return (
     <StyledDiv>
       <StyledLandingImg>
@@ -11,14 +33,132 @@ function LandingPage() {
         <StyledButton>
           Book now!
         </StyledButton>
-
       </StyledLandingImg>
+      <StyledRatingDiv>
+        <StyledHeadlineDiv>
+          <Styledh2headline1>
+            Why Dust
+          </Styledh2headline1>
+          <Styledh2headline2>
+            Busters?
+          </Styledh2headline2>
+        </StyledHeadlineDiv>
+        <StyledSpan></StyledSpan>
+        <StyledIconDiv>
+          <StyledIconsDiv>
+            hej
+          </StyledIconsDiv>
+          <StyledIconsDiv>
+            hej
+          </StyledIconsDiv>
+          <StyledIconsDiv>
+            <AiFillSafetyCertificate size={80} color='#FFD530' />
+          </StyledIconsDiv>
+        </StyledIconDiv>
+
+        <StyledSliderDiv>
+          <Slider  {...settings}>
+            <div >
+              <StyledSlides className='slides'>
+                <StyledRowDiv2>
+                    
+                    <StyledRatingh3>Anna87</StyledRatingh3>
+                </StyledRowDiv2>
+                <StyledP>
+                  "Grym städning och jättetrevlig personal tack så mycket!!"
+                </StyledP>
+               
+                <StyledRowDiv1>
+                  
+                  <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/>
+                
+                </StyledRowDiv1>
+                
+              </StyledSlides>
+            </div>
+            <div>
+            <StyledSlides className='slides'>
+                <StyledRowDiv2>
+                    
+                    <StyledRatingh3>Anna87</StyledRatingh3>
+                </StyledRowDiv2>
+                <StyledP>
+                  "Grym städning och jättetrevlig personal tack så mycket!!"
+                </StyledP>
+               
+                <StyledRowDiv1>
+                  
+                  <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/>
+                
+                </StyledRowDiv1>
+                
+              </StyledSlides>
+            </div>
+            <div>
+            <StyledSlides className='slides'>
+                <StyledRowDiv2>
+                    
+                    <StyledRatingh3>Anna87</StyledRatingh3>
+                </StyledRowDiv2>
+                <StyledP>
+                  "Grym städning och jättetrevlig personal tack så mycket!!"
+                </StyledP>
+               
+                <StyledRowDiv1>
+                  
+                  <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/>
+                
+                </StyledRowDiv1>
+                
+              </StyledSlides>
+            </div>
+            <div>
+            <StyledSlides className='slides'>
+                <StyledRowDiv2>
+                    
+                    <StyledRatingh3>Anna87</StyledRatingh3>
+                </StyledRowDiv2>
+                <StyledP>
+                  "Grym städning och jättetrevlig personal tack så mycket!!"
+                </StyledP>
+               
+                <StyledRowDiv1>
+                  
+                  <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/>
+                
+                </StyledRowDiv1>
+                
+              </StyledSlides>
+            </div>
+            <div>
+            <StyledSlides className='slides'>
+                <StyledRowDiv2>
+                    
+                    <StyledRatingh3>Anna87</StyledRatingh3>
+                </StyledRowDiv2>
+                <StyledP>
+                  "Grym städning och jättetrevlig personal tack så mycket!!"
+                </StyledP>
+               
+                <StyledRowDiv1>
+                  
+                  <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/> <AiFillStar color='#FFD530' size={30}/>
+                
+                </StyledRowDiv1>
+                
+              </StyledSlides>
+            </div>
+          </Slider>
+        </StyledSliderDiv>
+
+      </StyledRatingDiv>
     </StyledDiv>
   )
 }
 const StyledDiv = styled.div`
   width: 100%;
-  min-height: 100vh;
+  min-height: 300vh;
+  font-family: 'Poppins', sans-serif;
  
 `
 const StyledLandingImg = styled.div`
@@ -29,10 +169,10 @@ const StyledLandingImg = styled.div`
   justify-content: flex-start;
   flex-direction: column;
 
-  background-image: url('/public/imgs/pexels-tima-miroshnichenko-6195125.jpg');
+  background-image: url('/imgs/pexels-tima-miroshnichenko-6195125.jpg');
   background-size: cover;
   background-position: bottom;  
-  font-family: 'Poppins', sans-serif;
+  
 `
 const StyledHeadline = styled.h1`
 margin-top: 250px;
@@ -51,5 +191,137 @@ width: 180px;
 height: 50px;
 
 `
+const StyledRatingDiv = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #232323;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+`
+const StyledHeadlineDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  margin-top: 150px;
+
+`
+const Styledh2headline1 = styled.h2`
+
+font-size:40px;
+color: white;
+`
+const Styledh2headline2 = styled.h2`
+
+font-size:40px;
+color: #FFD530;
+`
+const StyledSpan = styled.span`
+height: 5px;
+width: 60px;
+background-color: #FFD530;
+border-radius: 10px;
+`
+const StyledIconDiv = styled.div`
+  width: 100%;
+  margin-top:80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 150px;
+  flex-direction: row;
+`
+const StyledIconsDiv = styled.div`
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 150px;
+  width: 150px;
+  border-radius: 75px;
+  background-color: white;
+  border: 1px solid white;
+`
+const StyledSliderDiv = styled.div`
+
+
+margin-top: 150px;
+
+padding:20px;
+
+height: 400px;
+width: 70%;
+
+
+
+
+
+`
+const StyledSlides = styled.div`
+ 
+
+  color: black;
+  background-color: white;
+  height: 200px;
+  width: 300px;
+  margin: auto;
+  border-radius: 10px;
+  border: 1px solid black;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+
+`
+
+const StyledRowDiv1 = styled.div`
+ 
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 10px;
+
+`
+const StyledRowDiv2 = styled.div`
+ 
+
+display: flex;
+align-items: center;
+justify-content: flex-start;
+
+width: 100%;
+gap: 10px;
+
+`
+const StyledRatingh3 = styled.h3`
+
+font-size:20px;
+margin-left: 5%;
+
+`
+const StyledP = styled.h3`
+
+font-size:15px;
+font-weight: normal;
+text-align: center;
+
+`
+const StyledRatingimg = styled.div`
+
+height: 40px;
+width: 40px;
+border-radius: 20px;
+border: 1px solid black;
+
+`
+
+;
+
 
 export default LandingPage
