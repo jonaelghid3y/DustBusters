552a3f597a3fe7a73ee163bd4683a90f148eca8a
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-09-27 09:54:40 +0200

    Added get call for services, not working #35

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 function Services() {
+
+  useEffect(() => {
+    fetchServices()
+  }, [])
+
+  const fetchServices = async () => {
+    try {
+      const response = await fetch('https://api-s5hih6nmta-uc.a.run.app/services')
+      const data = await response.json();
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
     <div className="servicesContainer">
-        services
+      services
     </div>
   )
 }
