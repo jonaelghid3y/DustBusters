c886f599aed67e5b069314bb192ec9d0b6c64a13
Leon Aysa <leon.basket@live.com>
2023-10-04 03:54:25 +0200

    profile pic added

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import AboutUs from './Pages/AboutUs';
 import Services from './Pages/Services';
 import ServicesAdmin from './Admin/ServicesAdmin';
 import Login from './Pages/Login';
+import Profile from './Pages/Profile';
 
 const router = createBrowserRouter([
   {
@@ -44,8 +45,11 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login/>
-
-      }
+      },
+      {
+        path: "/Profile",
+        element: <Profile/>,
+      },
     
 
 
