e99e4772cc2e543e1530551066528f615e27625f
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-10-05 16:52:01 +0200

    broke out login and register

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { Button } from '../Components/Button';
+
+const LoginForm = ({title, key, label, text, setIsRegistering, isRegistering}) => {
+
+  const toggleForm = () => {
+    setIsRegistering(!isRegistering);
+  };
+
+  return (
+    <motion.div
+      key={key}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}>
+      <StyledFormHeadline>
+        {title}
+      </StyledFormHeadline>
+      <StyledForm>
+        <StyledFormDivs>
+          <label> Username:</label>
+          <StyledInput type="text" name='username' />
+        </StyledFormDivs>
+        <StyledFormDivs>
+          <label> Password:</label>
+          <StyledInput type="password" name='password' />
+        </StyledFormDivs>
+        <StyledP>{text}<Link onClick={toggleForm}>Sign in</Link></StyledP>
+        <Button primary size="small" label={label} />
+      </StyledForm>
+    </motion.div>
+  );
+};
+
+const StyledFormHeadline = styled.h3`
+
+font-size: 30px;
+margin-bottom: 20px;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  border: 1px solid black
+  width: 80%;
+  height: 80%;
+  gap: 25px;
+`;
+
+const StyledFormDivs = styled.div`
+display: flex;
+align-items: flex-start;
+justify-content: center;
+flex-direction: column;
+`;
+
+const StyledInput = styled.input`
+font-size: 16.5px;
+width: 20vw;
+height: 5vh;
+@media (max-width: 768px) {
+  width: 60vw;
+ }
+`;
+const StyledP = styled.h3`
+font-size:15px;
+font-weight: normal;
+text-align: center;
+`;
+
+export default LoginForm;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,73 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '../Components/Button';
 import { motion, AnimatePresence } from 'framer-motion';
+import LoginForm from '../Components/LoginForm';
 
 function Login() {
 
   const [isRegistering, setIsRegistering] = useState(false);
 
-  const toggleForm = () => {
-    setIsRegistering(!isRegistering);
-  };
-
   return (
     <StyledDiv>
-      <StyledReviewModal>
+      <StyledLoginModal>
         <AnimatePresence mode='wait'>
-
           {isRegistering ? (
-            <motion.div
-              key="registerForm"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <StyledFormHeadline>
-                Register
-              </StyledFormHeadline>
-              <StyledForm>
-                <StyledFormDivs>
-                  <label> Username:</label>
-                  <StyledInput type="text" name='username' />
-                </StyledFormDivs>
-                <StyledFormDivs>
-                  <label> Password:</label>
-                  <StyledInput type="password" name='password' />
-                </StyledFormDivs>
-                <StyledP>Already have an account? <Link onClick={toggleForm}>Sign in</Link></StyledP>
-                <Button primary size="small" label="Register" />
-              </StyledForm>
-            </motion.div>
+            <LoginForm
+              title="Register"
+              label="Register"
+              key="registerform"
+              text="Already have an account? "
+              isRegistering={isRegistering}
+              setIsRegistering={setIsRegistering} />
           ) : (
-            <motion.div
-              key="loginForm"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <StyledFormHeadline>
-                Sign in
-              </StyledFormHeadline>
-              <StyledForm>
-                <StyledFormDivs>
-                  <label> Username:</label>
-                  <StyledInput type="text" name='username' />
-                </StyledFormDivs>
-                <StyledFormDivs>
-                  <label> Password:</label>
-                  <StyledInput type="password" name='password' />
-                </StyledFormDivs>
-                <StyledP>Don't have an account? <Link onClick={toggleForm}>Register</Link></StyledP>
-                <Button primary size="small" label="Log in" />
-              </StyledForm>
-            </motion.div>
+            <LoginForm
+              title="Log in"
+              label="Log in"
+              key="loginform"
+              text="Don't have an account? "
+              isRegistering={isRegistering}
+              setIsRegistering={setIsRegistering}
+            />
           )}
         </AnimatePresence>
-      </StyledReviewModal>
-
+      </StyledLoginModal>
     </StyledDiv>
   );
 }
@@ -82,7 +46,7 @@ const StyledDiv = styled.div`
   justify-content: center;
   background-color: #D9D9D9;
 `;
-const StyledReviewModal = styled(motion.div)`
+const StyledLoginModal = styled(motion.div)`
 
 border: 1px solid lightgray;
 background-color: white;
@@ -103,45 +67,6 @@ box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
   min-height: 600px;
   
  }
-`;
-const StyledForm = styled.form`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: flex-start;
-  border: 1px solid black
-  width: 80%;
-  height: 80%;
-  gap: 25px;
-`;
-const StyledFormHeadline = styled.h3`
-
-font-size: 30px;
-margin-bottom: 20px;
-`;
-
-const StyledFormDivs = styled.div`
-display: flex;
-align-items: flex-start;
-justify-content: center;
-flex-direction: column;
-`;
-
-const StyledInput = styled.input`
-font-size: 16.5px;
-width: 20vw;
-height: 5vh;
-@media (max-width: 768px) {
-
-  width: 60vw;
-  
- }
-
-`;
-const StyledP = styled.h3`
-font-size:15px;
-font-weight: normal;
-text-align: center;
 `;
 
 export default Login;
