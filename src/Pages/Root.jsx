dd5648b9decff53cd5676a0b596e395c01e29c5c
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-10-05 14:18:54 +0200

    fixed more lint errors

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
-import '../App.css'
-import { Outlet, useLocation } from 'react-router-dom'; 
-
-
-
-
+import '../App.css';
+import { Outlet } from 'react-router-dom';
 
 const Root = () => {
-   
-    return (
-        <>
-       
-            <Header   />
-            <Outlet   />
-            <Footer />
-      
-       
-        </>
-    );
+
+  return (
+    <>
+
+      <Header />
+      <Outlet />
+      <Footer />
+
+    </>
+  );
 };
 
 export default Root;
--- src/stories/ImageDivs.stories.js
+++ src/stories/ImageDivs.stories.js
@@ -1,86 +1,84 @@
-import { ImageDivs } from '../Components/ImageDivs'
+import { ImageDivs } from '../Components/ImageDivs';
 
 export default {
-    title: 'Example/Imagediv',
-    component: ImageDivs,
-    parameters: {
-        layout: 'centered',
-    },
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
+  title: 'Example/Imagediv',
+  component: ImageDivs,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 };
 
 export const largeImage1 = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6195278.jpg',
-        size: 'large'
-    }
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6195278.jpg',
+    size: 'large'
+  }
 };
-export const mediumImage1  = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6195278.jpg',
-        size: 'Medium'
-    }
-};
-
-export const smallImage1  = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6195278.jpg',
-        size: 'small'
-    }
+export const mediumImage1 = {
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6195278.jpg',
+    size: 'Medium'
+  }
 };
 
-
-export const largeImage2  = {
-    args:
-    {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6196685.jpg',
-        size: 'large'
-    }
+export const smallImage1 = {
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6195278.jpg',
+    size: 'small'
+  }
 };
 
-export const mediumImage2  = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6196685.jpg',
-        size: 'medium'
-    }
+export const largeImage2 = {
+  args:
+  {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6196685.jpg',
+    size: 'large'
+  }
 };
 
-export const smallImage2  = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6196685.jpg',
-        size: 'small'
-    }
+export const mediumImage2 = {
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6196685.jpg',
+    size: 'medium'
+  }
 };
 
-
-export const largeImage3  = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6197122.jpg',
-        size: 'large'
-    }
+export const smallImage2 = {
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6196685.jpg',
+    size: 'small'
+  }
 };
 
-export const mediumImage3  = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6197122.jpg',
-        size: 'medium'
-    }
+export const largeImage3 = {
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6197122.jpg',
+    size: 'large'
+  }
 };
 
-export const smallImage3  = {
-    args: {
-        primary: true,
-        src: '/imgs/pexels-tima-miroshnichenko-6197122.jpg',
-        size: 'small'
-    }
+export const mediumImage3 = {
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6197122.jpg',
+    size: 'medium'
+  }
+};
+
+export const smallImage3 = {
+  args: {
+    primary: true,
+    src: '/imgs/pexels-tima-miroshnichenko-6197122.jpg',
+    size: 'small'
+  }
 };
