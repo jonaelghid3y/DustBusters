e75ae83c99897258bfaa1258b35737fc86aa2d81
jonaelghid3y <jonaelghidey@hotmail.com>
2023-10-10 13:30:37 +0200

    responsiveness

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,27 +4,56 @@ import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 function AboutUs() {
+  const controls1 = useAnimation();
+  const [ref1, inView1] = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
+
+  const controls2 = useAnimation();
+  const [ref2, inView2] = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
+
+  React.useEffect(() => {
+    if (inView1) {
+      controls1.start({ opacity: 1, y: 0 });
+    }
+    if (inView2) {
+      controls2.start({ opacity: 1, y: 0 });
+    }
+  }, [controls1, controls2, inView1, inView2]);
   return (
     <StyledContainer>
       <StyledDiv>
         <StyledImage src='/imgs/pexels-tima-miroshnichenko-6197108.jpg' alt="DustBusters Cleaning" />
-        <StyledTextContainer>
-          <StyledText>
-            <BrandName1>Dust</BrandName1><BrandName2>Busters</BrandName2> is a highly experienced cleaning company with a dedicated and trained staff. Our flexibility and wide range of services allow us to provide both complete and partial cleaning solutions. We are based in Stockholm but offer our services nationwide.
-          </StyledText>
-          <StyledText>
-            We cater to private individuals by offering a range of services including everyday cleaning, window cleaning, final cleaning, and our specialty: <BrandName2>dust busting</BrandName2>. Whether the task is large or small, we are committed to providing comprehensive cleaning support!
-          </StyledText>
-        </StyledTextContainer>
+        <motion.div initial={{ opacity: 0, y: 50 }} transition={{delay: 0.5, ease: 'easeOut'}} animate={controls1} ref={ref1}>
+          <StyledTextContainer>
+
+            <StyledText>
+
+              <BrandName1>Dust</BrandName1><BrandName2>Busters</BrandName2> is a highly experienced cleaning company with a dedicated and trained staff. Our flexibility and wide range of services allow us to provide both complete and partial cleaning solutions. We are based in Stockholm but offer our services nationwide.
+
+            </StyledText>
+
+            <StyledText>
+              We cater to private individuals by offering a range of services including everyday cleaning, window cleaning, final cleaning, and our specialty: <BrandName2>dust busting</BrandName2>. Whether the task is large or small, we are committed to providing comprehensive cleaning support!
+            </StyledText>
+
+          </StyledTextContainer>
+        </motion.div>
       </StyledDiv>
       <StyledDiv style={{ backgroundColor: '#232323', color: 'white' }}>
         <StyledTextContainer>
-          <StyledText>
-            Our vision is to make cleaning services accessible to everyone, regardless of their location in the country. Therefore, it is important to us that you feel comfortable and confident in our services, including pricing, execution, and quality.
-          </StyledText>
-          <StyledText>
-            With <BrandName1>Dust</BrandName1><BrandName2>Busters</BrandName2>, cleaning will never feel like a chore. You can relax while we do the work for you. We are fully insured, licensed, and handle the RUT deduction for you, allowing you to benefit from a 50% deduction on labor costs.
-          </StyledText>
+          <motion.div initial={{ opacity: 0, y: 50 }} transition={{delay: 1, ease: 'easeOut'}}  animate={controls2} ref={ref2}>
+            <StyledText>
+              Our vision is to make cleaning services accessible to everyone, regardless of their location in the country. Therefore, it is important to us that you feel comfortable and confident in our services, including pricing, execution, and quality.
+            </StyledText>
+            <StyledText>
+              With <BrandName1>Dust</BrandName1><BrandName2>Busters</BrandName2>, cleaning will never feel like a chore. You can relax while we do the work for you. We are fully insured, licensed, and handle the RUT deduction for you, allowing you to benefit from a 50% deduction on labor costs.
+            </StyledText>
+          </motion.div>
         </StyledTextContainer>
       </StyledDiv>
     </StyledContainer>
@@ -52,6 +81,9 @@ const StyledImage = styled.img`
   height: auto;
   border-radius: 35px;
   margin-bottom: 2rem;
+  @media (max-width: 768px) {
+    width: 350px;
+}
 `;
 
 const StyledTextContainer = styled.div`
