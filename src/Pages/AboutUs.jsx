6ae883cf0ad09f3d8d15e7f58a8438fbf9ac49e6
Hilda Henriksson <hilda.henriksson@hotmail.com>
2023-10-10 11:42:15 +0200

    success

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+// import { motion, useAnimation } from 'framer-motion';
+// import { useInView } from 'react-intersection-observer';
 
 function AboutUs() {
   return (
